30ebed60cacb58019a3fe24c462071b11daa450f
AishaD <AishaDahir75@gmail.com>
2022-03-15 22:32:18 +0000

    remove sample ID after EGG2

--- EBH-929/anonymise_sample_names.sh
+++ EBH-929/anonymise_sample_names.sh
@@ -4,15 +4,12 @@
 # As the order of the fastq files do not follow the samplesheet order,
 # we can assign s1, s2 as we loop through the list of sample fastqs
 
-
-mkdir annonymised_outputs
 tmp=( $(ls | sort | uniq))
 len=( $(ls | wc -l) / 4)
+mkdir annonymised_outputs
 for j in `seq 0 4 $len`; 
     do echo $j; 
     for ((i=$j; i<$(($j + 4)); i++)); 
-        do echo ${tmp[$i]}; scp ${tmp[$i]}  annonymised_outputs/$(echo ${tmp[$i]} | sed "s/.*MYE/S${j}/g");
+        do echo ${tmp[$i]}; scp ${tmp[$i]}  annonymised_outputs/$(echo ${tmp[$i]} | sed "s/.*EGG2_//g" | sed "s/^[^_]*_/S${j}_/g");
     done;
 done
-
-
