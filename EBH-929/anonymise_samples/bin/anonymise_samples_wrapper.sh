db658a3b00bd1b29abdc11dad6111960e11dab02
AishaD <AishaDahir75@gmail.com>
2022-03-18 09:22:30 +0000

    add where to run script

--- EBH-929/anonymise_samples/bin/anonymise_samples_wrapper.sh
+++ EBH-929/anonymise_samples/bin/anonymise_samples_wrapper.sh
@@ -1,3 +1,6 @@
+## Run this script when current location is in the data file
+
+
 # tmp contains the list of files in current directory
 tmp=( $(ls | sort | uniq))
 # number of files to loop through, take one away as loop starts at 0
@@ -7,7 +10,7 @@ mkdir anonymised_outputs
 
 # As we need the sample names to be random, we will assign a random five
 # digit number
-# All output samples will be else in another folder called anonymised_outputs
+# All output samples will be elsewhere in another folder called anonymised_outputs
 for j in `seq 0 1 $len`;
     do echo ${tmp[$j]};
     random_num=${RANDOM:0:5};
