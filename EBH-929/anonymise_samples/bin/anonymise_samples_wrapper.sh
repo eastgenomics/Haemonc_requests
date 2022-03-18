9195b6826efb09cbda6c972a1a615bafd49df587
AishaD <AishaDahir75@gmail.com>
2022-03-18 10:29:39 +0000

    cut first column of file

--- EBH-929/anonymise_samples/bin/anonymise_samples_wrapper.sh
+++ EBH-929/anonymise_samples/bin/anonymise_samples_wrapper.sh
@@ -1,19 +1,12 @@
 ## Run this script when current location is in the data file
-
-
-# tmp contains the list of files in current directory
-tmp=( $(ls | sort | uniq))
-# number of files to loop through, take one away as loop starts at 0
-len=$(($(ls | wc -l)-1))
-
 mkdir anonymised_outputs
 
 # As we need the sample names to be random, we will assign a random five
 # digit number
 # All output samples will be elsewhere in another folder called anonymised_outputs
-for j in `seq 0 1 $len`;
-    do echo ${tmp[$j]};
+for j in $(find . -type f -name "*.tsv");
+    do echo $j;
     random_num=${RANDOM:0:5};
     echo S$random_num;
-    python ../bin/anonymise_samples.py -f ${tmp[$j]}  -o S${random_num}_markdup_recalibrated_tnhaplotyper2_allgenes -p anonymised_outputs
+    cut -f 2- $j > anonymised_outputs/S${random_num}_markdup_recalibrated_tnhaplotyper2_allgenes.tsv
 done > anonymised_outputs/log_out.txt
