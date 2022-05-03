abca78b0159358b12267d85d530ca0fe1345a4b6
AishaD <AishaDahir75@gmail.com>
2022-05-03 10:59:12 +0100

    URA-97 add description for bash script

--- URA-97/generate_probes.sh
+++ URA-97/generate_probes.sh
@@ -1,3 +1,8 @@
-# cut first three columns as the fourth contains genes for some probes
+# TWIST have sent two folders 1X and 2X of probes.
+# We need to merge these two probe files to get one for the whole
+# capture. This probe file is used in Picard which is required for
+# the multiQC report.
+
+# cut first three columns as the fourth col contains genes for some probes
 
 cat Probes_merged_ok_Cambridge_NHS_Myeloid_1X_TE-96527893_hg38_211119151014.bed Probes_merged_ok_Cambridge_NHS_Myeloid_2X_TE-96527893_hg38_211119151112.bed | cut -f 1,2,3 > Probes_GRCh38_HaemOnc_v2.0.bed 
