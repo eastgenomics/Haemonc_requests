a2edadd317f3e46ef1ed7face6451fdb463140fe
AishaD <AishaDahir75@gmail.com>
2022-05-04 11:12:09 +0100

    format and clear commenting

--- EBH-825/all_variants_tsv/get_samples.sh
+++ EBH-825/all_variants_tsv/get_samples.sh
@@ -1,13 +1,22 @@
 
 projects=$(dx find projects --name "002_*MYE")
 
-for project in $projects;  do      echo $project; file_id=$(dx find data --json --path="$project" --name="*_allgenes.tsv" | jq -r '.[].id');  for file in $file_id; do         dx download $file --no-progress; done; done
+for project in $projects; do
+    echo $project
+    file_ids=$(dx find data --json --path="$project" --name="*_allgenes.tsv" | jq -r '.[].id')
+    for file in $file_ids; do
+         dx download $file --no-progress
+    done
+done
 
-
-# ! The last column is wrapped so for everyline there is extra 5 rows, so to know wc -l 
-# we need to divide by 6 
+# ! The last column (summary text) is wrapped so for everyline there is
+# extra 5 rows coming from the summary text, so to know wc -l then
+# divide by 6
 
 # count number of lines in each file and add to count
+# count variable is used to check that all lines are correctly
+# concat without the header.
+
 count=0
 for file in *.tsv;
     do echo $file;
